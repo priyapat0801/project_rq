41c32d8bddd1fc77d133809ffb896fbb3f4bdae8
priyapat0801 <minkkyyyy@gmail.com>
2025-08-19 09:06:20 +0700

    Add files via upload

---
+++ project_rq/admin/db_connect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$db_username = "root";
+$db_password = ""; // รหัสผ่านของ MySQL
+$dbname = "mydb";  // เปลี่ยนชื่อฐานข้อมูลให้ถูกต้อง
+
+// สร้างการเชื่อมต่อ
+$conn = new mysqli($servername, $db_username, $db_password, $dbname);
+
+// ตรวจสอบการเชื่อมต่อ
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
